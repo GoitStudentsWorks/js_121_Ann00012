28c603eb115cfd2b2ff8c989fb2451a3b2679eec
SvitlanaKovshun <svetik_d@ukr.net>
2025-11-07 20:38:38 +0200

    mobile styles

--- src/js/faq-section.js
+++ src/js/faq-section.js
@@ -1,8 +1,4 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-new Accordion('.accordion-container', {
-  onOpen: function (currentElement) {
-    console.log(currentElement);
-  },
-});
+new Accordion('.accordion-container', {});
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
+// import Accordion from 'accordion-js';
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// import iziToast from 'izitoast';
+// import raty from 'raty-js';
+
 import Accordion from 'accordion-js';
-import Swiper, { Navigation, Pagination } from 'swiper';
-import iziToast from 'izitoast';
-import raty from 'raty-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container', {
+  duration: 1000,
+  ariaEnabled: true,
+  collapse: true,
+  elementClass: 'ac',
+  //     triggerClass: 'ac-trigger',
+  //   panelClass: 'ac-panel',
+  beforeOpen() {},
+});
