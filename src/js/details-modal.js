57a7f65e298a9ad53ca511e09e8f716e64e59f25
Ярослав Крохмальний <krokhmalniy.code@gmail.com>
2025-11-08 22:37:23 +0200

    Create close button logic

--- src/js/details-modal.js
+++ src/js/details-modal.js
@@ -1,3 +1,11 @@
-function name(params) {
-    
+export function addClassName(param) {
+  param.classList.add('is-open');
+}
+
+export function remoweClassName(param) {
+  param.classList.remove('is-open');
+}
+
+export function logFoo() {
+  console.log(this);
 }
--- src/main.js
+++ src/main.js
@@ -4,3 +4,21 @@ import { Navigation, Pagination } from 'swiper/modules';;
 import iziToast from 'izitoast';
 import raty from 'raty-js';
 
+// my code
+
+import { addClassName, remoweClassName  } from './js/details-modal';
+
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const closeButton = document.querySelector('.close-button');
+
+
+// Показує модальне вікно, кнопка ще не вибрана
+// closeButton.addEventListener('click', () => { 
+//   addClassName(modalOverlay);
+// }); 
+
+
+closeButton.addEventListener('click', () => {
+    remoweClassName(modalOverlay);
+});
